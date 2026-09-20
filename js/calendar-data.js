16daa854baf0abbcd984d5cfa739a87b60503ffc
Swrang Swargiary <sboro3235@gmail.com>
2026-09-20 14:06:52 +0530

    Create js/calendar-data.js

---
+++ js/calendar-data.js
@@ -0,0 +1,132 @@
+const bodoMonthsData = [
+  {
+    id: 0,
+    name: "Magh / Maghw (माघ)",
+    englishName: "Magh",
+    dateRange: "16 Jan – 15 Feb",
+    season: "Gozon / Winter",
+    startMonth: 0, // Jan
+    startDate: 16,
+    daysCount: 31
+  },
+  {
+    id: 1,
+    name: "Fagun (फागुन)",
+    englishName: "Fagun",
+    dateRange: "16 Feb – 15 Mar",
+    season: "Gozon / Late Winter",
+    startMonth: 1, // Feb
+    startDate: 16,
+    daysCount: 28
+  },
+  {
+    id: 2,
+    name: "Chaitra (चैत्र)",
+    englishName: "Chaitra",
+    dateRange: "16 Mar – 14 Apr",
+    season: "Bwisagu / Early Spring",
+    startMonth: 2, // Mar
+    startDate: 16,
+    daysCount: 30
+  },
+  {
+    id: 3,
+    name: "Bwisag (बैसागो / बैसाग)",
+    englishName: "Bwisag",
+    dateRange: "15 Apr – 15 May",
+    season: "Bwisag Ritu (Spring Season)",
+    startMonth: 3, // Apr
+    startDate: 15,
+    daysCount: 31
+  },
+  {
+    id: 4,
+    name: "Jeth (जेथो)",
+    englishName: "Jeth",
+    dateRange: "16 May – 15 Jun",
+    season: "Dungfu / Early Summer",
+    startMonth: 4, // May
+    startDate: 16,
+    daysCount: 31
+  },
+  {
+    id: 5,
+    name: "Aasar (आसार)",
+    englishName: "Aasar",
+    dateRange: "16 Jun – 15 Jul",
+    season: "Akhra / Monsoon",
+    startMonth: 5, // Jun
+    startDate: 16,
+    daysCount: 30
+  },
+  {
+    id: 6,
+    name: "Sawan (सावन)",
+    englishName: "Sawan",
+    dateRange: "16 Jul – 15 Aug",
+    season: "Akhra / Rainy Season",
+    startMonth: 6, // Jul
+    startDate: 16,
+    daysCount: 31
+  },
+  {
+    id: 7,
+    name: "Bhadra (भाद्र)",
+    englishName: "Bhadra",
+    dateRange: "16 Aug – 15 Sep",
+    season: "Late Monsoon",
+    startMonth: 7, // Aug
+    startDate: 16,
+    daysCount: 31
+  },
+  {
+    id: 8,
+    name: "Aasin (आसिन)",
+    englishName: "Aasin",
+    dateRange: "16 Sep – 15 Oct",
+    season: "Sirri / Autumn",
+    startMonth: 8, // Sep
+    startDate: 16,
+    daysCount: 30
+  },
+  {
+    id: 9,
+    name: "Kati (खाथि)",
+    englishName: "Kati",
+    dateRange: "16 Oct – 15 Nov",
+    season: "Sirri / Late Autumn",
+    startMonth: 9, // Oct
+    startDate: 16,
+    daysCount: 31
+  },
+  {
+    id: 10,
+    name: "Aghon (आघन)",
+    englishName: "Aghon",
+    dateRange: "16 Nov – 15 Dec",
+    season: "Gozon / Pre-Winter",
+    startMonth: 10, // Nov
+    startDate: 16,
+    daysCount: 30
+  },
+  {
+    id: 11,
+    name: "Push (फुष)",
+    englishName: "Push",
+    dateRange: "16 Dec – 15 Jan",
+    season: "Gozon / Peak Winter",
+    startMonth: 11, // Dec
+    startDate: 16,
+    daysCount: 31
+  }
+];
+
+const bodoWeekdays = [
+  { en: "Sunday", bodo: "Rabibar (रबिबार)" },
+  { en: "Monday", bodo: "Sombar (समबार)" },
+  { en: "Tuesday", bodo: "Mongolbar (मंगलबार)" },
+  { en: "Wednesday", bodo: "Budhbar (बुधबार)" },
+  { en: "Thursday", bodo: "Brihospatibar (बृहस्पतीबार)" },
+  { en: "Friday", bodo: "Sakrubar (शक्रुबार)" },
+  { en: "Saturday", bodo: "Shanibar (शनिबार)" }
+];
