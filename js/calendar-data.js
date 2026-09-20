d362c0e3b82153f9ab3ff60f62fc84f6bb61df73
Swrang Swargiary <sboro3235@gmail.com>
2026-09-20 16:08:35 +0530

    Update calendar-data.js

--- js/calendar-data.js
+++ js/calendar-data.js
@@ -3,67 +3,67 @@ const bodoMonthsData = [
     id: 0,
     name: "Magh / Maghw (माघ)",
     englishName: "Magh",
-    dateRange: "16 Jan – 15 Feb",
+    dateRange: "15 Jan – 14 Feb",
     season: "Gozon / Winter",
-    startGregMonth: 0, // Jan (0-indexed)
-    startGregDay: 16,
+    startGregMonth: 0, // Jan
+    startGregDay: 15,
     daysCount: 31
   },
   {
     id: 1,
     name: "Fagun (फागुन)",
     englishName: "Fagun",
-    dateRange: "16 Feb – 15 Mar",
+    dateRange: "15 Feb – 14 Mar",
     season: "Gozon / Late Winter",
     startGregMonth: 1, // Feb
-    startGregDay: 16,
+    startGregDay: 15,
     daysCount: 28
   },
   {
     id: 2,
     name: "Chaitra (चैत्र)",
     englishName: "Chaitra",
-    dateRange: "16 Mar – 14 Apr",
+    dateRange: "15 Mar – 13 Apr",
     season: "Bwisagu / Early Spring",
     startGregMonth: 2, // Mar
-    startGregDay: 16,
+    startGregDay: 15,
     daysCount: 30
   },
   {
     id: 3,
     name: "Bwisag (बैसागो / बैसाग)",
     englishName: "Bwisag",
-    dateRange: "15 Apr – 15 May",
+    dateRange: "14 Apr – 14 May",
     season: "Bwisag Ritu (Spring Season)",
     startGregMonth: 3, // Apr
-    startGregDay: 15,
+    startGregDay: 14,
     daysCount: 31
   },
   {
     id: 4,
     name: "Jeth (जेथो)",
     englishName: "Jeth",
-    dateRange: "16 May – 15 Jun",
+    dateRange: "15 May – 14 Jun",
     season: "Dungfu / Early Summer",
     startGregMonth: 4, // May
-    startGregDay: 16,
+    startGregDay: 15,
     daysCount: 31
   },
   {
     id: 5,
     name: "Aasar (आसार)",
     englishName: "Aasar",
-    dateRange: "16 Jun – 15 Jul",
+    dateRange: "15 Jun – 15 Jul",
     season: "Akhra / Monsoon",
     startGregMonth: 5, // Jun
-    startGregDay: 16,
-    daysCount: 30
+    startGregDay: 15,
+    daysCount: 31
   },
   {
     id: 6,
     name: "Sawan (सावन)",
     englishName: "Sawan",
-    dateRange: "16 Jul – 15 Aug",
+    dateRange: "16 Jul – 16 Aug",
     season: "Akhra / Rainy Season",
     startGregMonth: 6, // Jul
     startGregDay: 16,
@@ -73,51 +73,51 @@ const bodoMonthsData = [
     id: 7,
     name: "Bhadra (भाद्र)",
     englishName: "Bhadra",
-    dateRange: "16 Aug – 15 Sep",
+    dateRange: "17 Aug – 18 Sep",
     season: "Late Monsoon",
     startGregMonth: 7, // Aug
-    startGregDay: 16,
-    daysCount: 31
+    startGregDay: 17,
+    daysCount: 32
   },
   {
     id: 8,
     name: "Aasin (आसिन)",
     englishName: "Aasin",
-    dateRange: "16 Sep – 15 Oct",
+    dateRange: "19 Sep – 18 Oct",
     season: "Sirri / Autumn",
     startGregMonth: 8, // Sep
-    startGregDay: 16,
+    startGregDay: 19, // 19 Sep = 1 Aasin, 20 Sep = 2 Aasin
     daysCount: 30
   },
   {
     id: 9,
     name: "Kati (खाथि)",
     englishName: "Kati",
-    dateRange: "16 Oct – 15 Nov",
+    dateRange: "19 Oct – 18 Nov",
     season: "Sirri / Late Autumn",
     startGregMonth: 9, // Oct
-    startGregDay: 16,
-    daysCount: 31
+    startGregDay: 19,
+    daysCount: 30
   },
   {
     id: 10,
     name: "Aghon (आघन)",
     englishName: "Aghon",
-    dateRange: "16 Nov – 15 Dec",
+    dateRange: "19 Nov – 18 Dec",
     season: "Gozon / Pre-Winter",
     startGregMonth: 10, // Nov
-    startGregDay: 16,
+    startGregDay: 19,
     daysCount: 30
   },
   {
     id: 11,
     name: "Push (फुष)",
     englishName: "Push",
-    dateRange: "16 Dec – 15 Jan",
+    dateRange: "19 Dec – 14 Jan",
     season: "Gozon / Peak Winter",
     startGregMonth: 11, // Dec
-    startGregDay: 16,
-    daysCount: 31
+    startGregDay: 19,
+    daysCount: 27
   }
 ];
 
